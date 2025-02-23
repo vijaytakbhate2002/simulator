07ffd880bf5915f9a33678e1aa768933c4670746
Vijay-Takbhate-incred <vijay.takbhate@incred.com>
2025-02-23 16:55:11 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,56 @@
+import io
+import os
+from pathlib import Path
+from setuptools import find_packages, setup
+
+NAME = 'simulation'
+DESCRIPTION = "Compare each common columns from two csv's and return comparision of each column and comparison flag for each column which is TRUE or FALSE"
+
+URL = 'https://github.com/Vijay-Takbhate-incred/simulator.git'
+EMAIL = 'vijay.takbhate@incred.com'
+AUTHOR = 'Vijay Dipak Takbhate'
+REQUIRES_PYTHON = '>=3.13'
+
+pwd = os.path.abspath(os.path.dirname(__file__))
+
+def list_reqs(fname='requirements.txt'):
+    try:
+        with io.open(os.path.join(pwd, fname), encoding='utf-8') as f:
+            return f.read().splitlines()
+    except FileNotFoundError:
+        pass
+
+try:
+    with io.open(os.path.join(pwd, 'README.md'), encoding='utf-8') as f:
+        long_description = f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+ROOT_DIR = Path(__file__).resolve().parent
+PACKAGE_DIR = ROOT_DIR / NAME
+
+setup(
+    name=NAME,
+    version="1.0.0",
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(exclude=('tests',)),
+    package_data={NAME: ['VERSION']},
+    install_requires=list_reqs(),
+    extras_require={},
+    include_package_data=True,
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
+)
